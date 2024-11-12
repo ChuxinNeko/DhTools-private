b3f61619e1f805702bece0102c081555fdefdd25
初心浮梦 <61831881+cxfm666@users.noreply.github.com>
2024-11-13 00:58:01 +0800

    Update Morax

--- src/App.vue
+++ src/App.vue
@@ -12,21 +12,26 @@
   <!-- 对话框内容 -->
   <a-modal 
     v-model:visible="visible" 
-    title="使用须知" 
+    title="梦乡公益服远程指令生成器-使用须知" 
     :maskClosable="false"
     :closable="false"
     :width="isMobile ? '90%' : '500px'"
     :top="isMobile ? '20px' : '60px'"
   >
     <div>
-      <p>本指令生成器为独立项目，不会以任何形式收费，也不会作为任何付费服务器的附赠品。出现在拼多多闲鱼等平台的均为倒卖！</p>
-      <p>勾选下方注意事项并点击确定方可开始使用：</p>
+      <p>本项目为独立开源项目，项目地址:<a href="https://github.com/lctoolsweb/DanhengWebTools" target="_blank">DanhengWebTools</a>。</p>
+      <p>项目不会以任何形式收费，也不会作为任何付费服务器的赠送品。</p>
+      <p>您在阅读后勾选下方注意事项并点击确定方可开始使用：</p>
       <a-checkbox v-model="checkbox1">
-        <span :style="{ color: checkboxTextColor }">我已知晓梦乡服务器和本指令生成器都是免费的。</span>
+        <span :style="{ color: checkboxTextColor }">我已知晓梦乡服务器和本指令生成器都是免费的，且绝不会以任何形式进行收费。</span>
       </a-checkbox>
       <br />
       <a-checkbox v-model="checkbox2">
-        <span :style="{ color: checkboxTextColor }">当我付费得到本项目并遇到问题时，我会主动询问卖家而不是询问开发者。</span>
+        <span :style="{ color: checkboxTextColor }">当我付费得到本项目并遇到问题时，我会主动询问卖家而不是用颐指气使且蛮横的态度来质问开发者。</span>
+      </a-checkbox>
+      <br />
+      <a-checkbox v-model="checkbox3">
+        <span :style="{ color: checkboxTextColor }">本站对接的是梦乡公益服，其他服务器请勿询问任何本站的使用问题，谢谢合作！</span>
       </a-checkbox>
     </div>
     <template #footer>
@@ -47,23 +52,64 @@ import Watermark from './components/Watermark.vue';
 import HeaderTop from './components/Header/HeaderTop.vue';
 import HeaderMenu from './components/Header/HeaderMenu.vue';
 import { Message } from '@arco-design/web-vue';
+import Cookies from 'js-cookie';
 
 const appStore = useAppStore();
 const apptheme = ref();
-const visible = ref(true); // 初始化为 true，页面加载时显示对话框
+const visible = ref(false); // 初始化为 false，页面加载时判断是否显示对话框
 const checkbox1 = ref(false);
 const checkbox2 = ref(false);
+const checkbox3 = ref(false);
 
 // 计算属性：检测是否为移动端
 const isMobile = computed(() => {
   return window.innerWidth < 768; // 根据屏幕宽度判断
 });
 const checkboxTextColor = computed(() => {
-  return checkbox1.value && checkbox2.value ? 'green' : 'red';
+  return checkbox1.value && checkbox2.value && checkbox3.value ? 'green' : 'red';
 });
 
-// 显式暴露给模板
-defineExpose({ isMobile });
+// 群链接数组
+const groupLinks = [
+  "https://qm.qq.com/q/a1qDFAGN3O", "https://qm.qq.com/q/jNrna23Qr0", "https://qm.qq.com/q/rJD0ikUUWk", "https://qm.qq.com/q/x81EKFu2OW", "https://qm.qq.com/q/eHclKGip2M",
+  "https://qm.qq.com/q/NxnCj3M2I0", "https://qm.qq.com/q/WC6NhUA2oE", "https://qm.qq.com/q/hzZiBo36jC", "https://qm.qq.com/q/tG7Wxe1JiU", "https://qm.qq.com/q/tSEkTfMV5m",
+  "https://qm.qq.com/q/NZgBTRtocO", "https://qm.qq.com/q/lPK5dFBeUM", "https://qm.qq.com/q/YR1EiqKjYY", "https://qm.qq.com/q/kBNvfWIPhm", "https://qm.qq.com/q/gGAWeEogVM"
+];
+
+// 选择随机链接
+const getRandomGroupLink = () => {
+  const randomIndex = Math.floor(Math.random() * groupLinks.length);
+  return groupLinks[randomIndex];
+};
+
+// 判断弹窗显示次数到次数后不再继续弹
+onMounted(() => {
+  const popupCount = parseInt(Cookies.get("popupCount") || "0", 10);
+  if (popupCount < 3) {
+    visible.value = true;
+    Cookies.set("popupCount", (popupCount + 1).toString(), { expires: 365 });
+  }
+
+  const WSS = localStorage.getItem("WSS");
+  if (WSS) {
+    appStore.socketConnect(WSS);
+  }
+});
+
+// 加入群聊
+const joingroup = () => {
+  const randomLink = getRandomGroupLink(); // 随机拿一个加群链接
+  window.open(randomLink, "_blank");
+};
+
+// 确认按钮操作
+const handleOk = () => {
+  if (!checkbox1.value || !checkbox2.value || !checkbox3.value) {
+    Message.error('请仔细阅读注意事项并勾选所有选项。');
+    return;
+  }
+  visible.value = false; // 确保只有点击确定按钮后关闭对话框
+};
 
 watch(
   () => appStore.theme,
@@ -79,27 +125,6 @@ watch(
     immediate: true,
   },
 );
-
-onMounted(() => {
-  const WSS = localStorage.getItem("WSS");
-  if (WSS) {
-    appStore.socketConnect(WSS);
-  }
-});
-
-const handleOk = () => {
-  if (!checkbox1.value || !checkbox2.value) {
-    Message.error('请仔细阅读注意事项并勾选所有选项。');
-    return;
-  }
-  visible.value = false; // 确保只有点击确定按钮后关闭对话框
-};
-const joingroup = () => {
-  window.open(
-    "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=JI5ZPpenP8i1e8fJj4zHDcgoJdYlaPyE&authKey=Od2ihJ5JGRd6vu5PVc1t%2Buiu0Uwov2O1JRYJmbYozcROGxQrrlZojHk40jMrEhyR&noverify=0&group_code=859381480",
-    "_blank"
-  );
-};
 
 </script>
 
@@ -168,4 +193,3 @@ const joingroup = () => {
   }
 }
 </style>
-
